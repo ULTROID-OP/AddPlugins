2d36a710da0b9d52ac69820a5dc9823de2ab46f5
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-03 20:24:15 +0530

    Update amongus.py

--- amongus.py
+++ amongus.py
@@ -5,14 +5,11 @@
 
 import asyncio
 
-from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot import ALIVE_NAME, CMD_HELP
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
+DEFAULTUSER = bot.me.first_name
 
 
-@bot.on(admin_cmd(pattern="imp(|n) (.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="imp(|n) (.*)", allow_sudo=True))
+@ultroid_cmd(pattern="imp(|n)")
 async def _(event):
     legendx22 = bot.uid
     USERNAME = f"tg://user?id={legendx22}"
