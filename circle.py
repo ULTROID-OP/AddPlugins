d7d00f00c55c5e46426c82c2446d0118e63e61f6
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-04 10:17:54 +0530

    Update circle.py

--- circle.py
+++ circle.py
@@ -3,7 +3,7 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.utils import admin_cmd,sudo_cmd
 from userbot import CMD_HELP
 
 
