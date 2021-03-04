8ed56bd81ea1666cfb80959f885fdceb6cffc1da
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-04 15:36:39 +0530

    Add files via upload

--- zombies.py
+++ zombies.py
@@ -1,17 +1,32 @@
-"""
-✘ Commands Available
-• `{i}zombies`
-    Gives the Number of Deleted Accounts.
-    
-• `{i}zombies clean`
-    Remove the deleted accounts if the user is admin.
-"""
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+"""Cmd= `.zombie`
+Usage: Searches for deleted accounts in a groups and channels.
+Use .zombies clean to remove deleted accounts from the groups and channels.
+\nPorted by ©[NIKITA](t.me/kirito6969) and ©[Bajrang Hellboy](t.me/legendx22)"""
+
+from telethon import events
+from userbot.utils import admin_cmd
+#
 from asyncio import sleep
-from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
-from telethon.tl.functions.channels import EditBannedRequest
-from telethon.tl.types import ChatBannedRights
-from . import *
+from os import remove
+
+from telethon.errors import (BadRequestError, ChatAdminRequiredError,
+                             ImageProcessFailedError, PhotoCropSizeSmallError,
+                             UserAdminInvalidError)
+from telethon.errors.rpcerrorlist import (UserIdInvalidError,
+                                          MessageTooLongError)
+from telethon.tl.functions.channels import (EditAdminRequest,
+                                            EditBannedRequest,
+                                            EditPhotoRequest)
+from telethon.tl.types import (ChannelParticipantsAdmins, ChatAdminRights,
+                               ChatBannedRights, MessageEntityMentionName,
+                               MessageMediaPhoto)
+
+
+# =================== CONSTANT ===================
 
 BANNED_RIGHTS = ChatBannedRights(
     until_date=None,
@@ -37,70 +52,79 @@ UNBAN_RIGHTS = ChatBannedRights(
 )
 
 MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
-UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 
+UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 # ================================================
 
 
-@ultroid_cmd(pattern='zombies ?(.*)')
+
+@borg.on(admin_cmd(pattern=f"zombies", allow_sudo=True))
+@borg.on(events.NewMessage(pattern="^.zombies(?: |$)(.*)", outgoing=True))
 async def rm_deletedacc(show):
+    """ For .zombies command, list all the ghost/deleted/zombie accounts in a chat. """
+
     con = show.pattern_match.group(1).lower()
     del_u = 0
     del_status = "`No deleted accounts found, Group is clean`"
+
     if con != "clean":
-        eh = await eor(show, "`Searching for ghost/deleted/zombie accounts...`")
-        async for user in ultroid_bot.iter_participants(show.chat_id):
+        await show.edit("`Searching for ghost/deleted/zombie accounts...`")
+        async for user in show.client.iter_participants(show.chat_id):
+
             if user.deleted:
                 del_u += 1
                 await sleep(1)
         if del_u > 0:
-            del_status = f"`Found` {del_u} `ghost/deleted/zombie account(s) in this group,\
-            \nClean them by using` `zombies clean`"
-        await eh.edit(del_status)
+            del_status = f"`Found` **{del_u}** `ghost/deleted/zombie account(s) in this group,\
+            \nclean them by using .zombies clean`"
+        await show.edit(del_status)
         return
+
+    # Here laying the sanity check
     chat = await show.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
+
     # Well
     if not admin and not creator:
-        await eor(show, "`I am not an admin here!`")
+        await show.edit("`I am not an admin here!`")
         return
-    ehh = await eor(show, "`Deleting deleted accounts...`")
+
+    await show.edit("`Deleting deleted accounts...\nOh I can do that?!?!`")
     del_u = 0
     del_a = 0
-    async for user in ultroid_bot.iter_participants(show.chat_id):
+
+    async for user in show.client.iter_participants(show.chat_id):
         if user.deleted:
             try:
-                await ultroid_bot(
-                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS)
-                )
+                await show.client(
+                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS))
             except ChatAdminRequiredError:
-                await eh.edit("`I don't have ban rights in this group`")
-                return
-            except UserAdminInvalidError:
-                del_u -= 1
-    del_a = 0
-    async for user in ultroid_bot.iter_participants(show.chat_id):
-        if user.deleted:
-            try:
-                await ultroid_bot(
-                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS)
-                )
-            except ChatAdminRequiredError:
-                await eh.edit("`I don't have ban rights in this group`")
+                await show.edit("`I don't have ban rights in this group`")
                 return
             except UserAdminInvalidError:
                 del_u -= 1
                 del_a += 1
-            await ultroid_bot(EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
+            await show.client(
+                EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
             del_u += 1
+
+
     if del_u > 0:
         del_status = f"Cleaned **{del_u}** deleted account(s)"
+
     if del_a > 0:
         del_status = f"Cleaned **{del_u}** deleted account(s) \
         \n**{del_a}** deleted admin accounts are not removed"
-    await ehh.edit(del_status)
+
+
+    await show.edit(del_status)
     await sleep(2)
     await show.delete()
-    
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=Var.HNDLR)}"})
+
+
+    if Config.G_BAN_LOGGER_GROUP is not None:
+        await show.client.send_message(
+            Config.G_BAN_LOGGER_GROUP, "#CLEANUP\n"
+            f"Cleaned **{del_u}** deleted account(s) !!\
+            \nCHAT: {show.chat.title}(`{show.chat_id}`)")
