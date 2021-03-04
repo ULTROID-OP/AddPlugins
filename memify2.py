6b04bb65e1d9ecdd89424c9aa0a024ac6a8467fe
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-04 15:30:54 +0530

    Add files via upload

--- memify2.py
+++ memify2.py
@@ -1,106 +1,180 @@
-# For The-TG-Bot-3.0
-# By Priyam Kalra
-# Parts of the code below is taken from other sources, the links to the sources is commented above the taken code
+"""Reply to an image/sticker with .mmf` 'text on top' ; 'text on bottom
+forked by: @legendx22
+created by: @A_Dark_Princ3
+"""
 
 from PIL import Image, ImageFont, ImageDraw
-import textwrap
-import os
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd, sudo_cmd
-from var import Var
-# how a lazy guy ports.
-client = borg
-@borg.on(admin_cmd("memify ?(.*)"))
+
+import textwrap, os
+from userbot.utils import admin_cmd
+
+
+
+
+
+@borg.on(admin_cmd(pattern=r"mmf ?(.*)"))
+
 async def handler(event):
+
     if event.fwd_from:
+
         return
+
     if not event.reply_to_msg_id:
-        await event.edit("You might want to try `.help memify`")
+
+        await event.reply("Usage:- `memify upper text ; lower text`")
+
         return
+
     reply_message = await event.get_reply_message()
+
     if not reply_message.media:
-        await event.edit("```Reply to a image/sticker.```")
+
+        await event.reply("Reply to a image/sticker")
+
         return
-    file = await client.download_media(reply_message, Var.TEMP_DOWNLOAD_DIRECTORY)
-    await event.edit("```Memifying this image! (」ﾟﾛﾟ)｣ ```")
+
+    file = await borg.download_media(reply_message, Var.TEMP_DOWNLOAD_DIRECTORY)
+
+    a = await event.reply("Hahaha...Memifying this image....")
+
     text = str(event.pattern_match.group(1)).strip()
+
     if len(text) < 1:
-        return await event.edit("You might want to try `.help memify`")
+
+        return await a.edit("Usage:- `memify upper text ; lower text`")
+
     meme = await drawText(file, text)
-    await client.send_file(event.chat_id, file=meme, force_document=False)
+
+    await event.client.send_file(event.chat_id, file=meme, force_document=False)
+
     os.remove(meme)
 
-# Taken from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/memify.py#L64
-# Maybe edited to suit the needs of this module
+    await event.delete()
+
+    await a.delete()
+
+
 
 
 async def drawText(image_path, text):
+
     img = Image.open(image_path)
+
     os.remove(image_path)
+
     shadowcolor = "black"
+
     i_width, i_height = img.size
+
     if os.name == "nt":
+
         fnt = "arial.ttf"
+
     else:
-        fnt = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+
+        fnt = "./Resources/Vermin Vibes V.otf"
+
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
-    if "," in text:
-        upper_text, lower_text = text.split(",")
+
+    if ";" in text:
+
+        upper_text, lower_text = text.split(";")
+
     else:
+
         upper_text = text
+
         lower_text = ''
+
     draw = ImageDraw.Draw(img)
+
     current_h, pad = 10, 5
+
     if upper_text:
+
         for u_text in textwrap.wrap(upper_text, width=15):
+
             u_width, u_height = draw.textsize(u_text, font=m_font)
+
             draw.text(xy=(((i_width - u_width) / 2) - 2, int((current_h / 640)*i_width)),
+
                       text=u_text, font=m_font, fill=(0, 0, 0))
+
             draw.text(xy=(((i_width - u_width) / 2) + 2, int((current_h / 640)*i_width)),
+
                       text=u_text, font=m_font, fill=(0, 0, 0))
+
             draw.text(xy=((i_width - u_width) / 2, int(((current_h / 640)*i_width)) - 2),
+
                       text=u_text, font=m_font, fill=(0, 0, 0))
+
             draw.text(xy=(((i_width - u_width) / 2), int(((current_h / 640)*i_width)) + 2),
+
                       text=u_text, font=m_font, fill=(0, 0, 0))
+
+
 
             draw.text(xy=((i_width - u_width) / 2, int((current_h / 640)*i_width)),
+
                       text=u_text, font=m_font, fill=(255, 255, 255))
+
             current_h += u_height + pad
+
     if lower_text:
+
         for l_text in textwrap.wrap(lower_text, width=15):
+
             u_width, u_height = draw.textsize(l_text, font=m_font)
+
             draw.text(
+
                 xy=(((i_width - u_width) / 2) - 2, i_height -
+
                     u_height - int((20 / 640)*i_width)),
+
                 text=l_text, font=m_font, fill=(0, 0, 0))
+
             draw.text(
+
                 xy=(((i_width - u_width) / 2) + 2, i_height -
+
                     u_height - int((20 / 640)*i_width)),
+
                 text=l_text, font=m_font, fill=(0, 0, 0))
+
             draw.text(
+
                 xy=((i_width - u_width) / 2, (i_height -
+
                                               u_height - int((20 / 640)*i_width)) - 2),
+
                 text=l_text, font=m_font, fill=(0, 0, 0))
+
             draw.text(
+
                 xy=((i_width - u_width) / 2, (i_height -
+
                                               u_height - int((20 / 640)*i_width)) + 2),
+
                 text=l_text, font=m_font, fill=(0, 0, 0))
 
+
+
             draw.text(
+
                 xy=((i_width - u_width) / 2, i_height -
+
                     u_height - int((20 / 640)*i_width)),
+
                 text=l_text, font=m_font, fill=(255, 255, 255))
+
             current_h += u_height + pad
+
     image_name = "memify.webp"
+
     webp_file = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, image_name)
+
     img.save(webp_file, "webp")
+
     return webp_file
-CMD_HELP.update(
-    {
-        "memify": ".memify (text)\
-\nUsage: print your text on photo or sticker.\
-"
-    }
-)
-
-
